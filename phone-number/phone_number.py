2482a1595b86f3bc1b7ccb901aa1bff8dd343db6
Blake Sager <blake.sager@activeprospect.com>
2019-08-06 22:14:23 -0500

    add phone-number exercise

---
+++ phone-number/phone_number.py
@@ -0,0 +1,26 @@
+class Phone(object):
+    def __init__(self, phone_number):
+        filtered = filter(lambda c: c.isnumeric(), phone_number)
+        self.number = ''.join(filtered)
+        numberLength = len(self.number)
+
+        if numberLength < 10 or numberLength > 11:
+            raise ValueError('Invalid phone number')
+        elif numberLength == 11 and self.number[0] != '1':
+            raise ValueError('Invalid phone number')
+        elif numberLength == 11:
+            self.number = self.number[1:]
+
+        self.area_code = self.number[0:3]
+        self.exchange_code = self.number[3:6]
+        self.line_number = self.number[-4:]
+        if int(self.area_code[0]) < 2 or int(self.exchange_code[0]) < 2:
+            raise ValueError('Invalid phone number')
+
+    def pretty(self):
+        return f"({self.area_code}) {self.exchange_code}-{self.line_number}"
+
+        
+
+# p = Phone("12234567890")
+# p.area_code
